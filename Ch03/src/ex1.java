fbdb8aa0ad3471d6d431dd9bac32ae57e7ea81f9
jns0720 <powwwww44@gmail.com>
2023-09-20 21:54:15 +0900

    java study
    ch03

---
+++ Ch03/src/ex1.java
@@ -0,0 +1,13 @@
+public class ex1 {
+
+    public static void main(String[] args) {
+        int x = -100;
+        x = -x;
+        System.out.println("x: " + x);
+
+        byte b = 100;
+        int y = -b;
+        System.out.println("y: " + y);
+    }
+
+}
